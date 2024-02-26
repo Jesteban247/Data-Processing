7cf81b9be8a34f34265d774dd6008ac4a2ec768d
Esteban <135396733+Jesteban247@users.noreply.github.com>
2024-02-26 15:55:20 +0000

    Test 2

--- .github/workflows/API/api.py
+++ .github/workflows/API/api.py
@@ -77,4 +77,5 @@ print(df_weather.head())
 # Finally, save the DataFrame to a JSON file with the current date in the filename
 current_date = datetime.now().strftime("%d-%m-%Y")
 file_name = f"{current_date}_data.json"
-df_weather.to_json('/workspaces/Data-Processing/Project/API/' + file_name, orient='records')
+df_weather.to_json('/workspaces/Data-Processing/Project/Data/' + file_name, orient='records')
+
